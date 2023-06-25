cc6b200bf52b7dabd9b7daf38ead620dea793f11
Alison Myers <alison.myers@sauder.ubc.ca>
2023-06-24 21:05:27 -0700

    customize config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,11 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://alisonlouisemyers.com", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
-          shortName: "Portfolio", // Used in manifest.json
+          siteName: "The Portfolio of Alison Louise Myers", // Used in manifest.json
+          shortName: "A.M. Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,10 @@ export default function IndexPage() {
       <Seo title="Gatsby Starter for Portfolio Minimal" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
+        <AboutSection sectionId="about" heading="About Me" />
+        <InterestsSection sectionId="interests" heading="Interests" />
         <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <ContactSection sectionId="github" heading="Questions?" />
       </Page>
     </>
   );
